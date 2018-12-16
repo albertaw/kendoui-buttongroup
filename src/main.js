000490ed7a057e87526e829918c439eb8460e5d3
Alberta Williams <alberta04@gmail.com>
2018-12-15 21:49:13 -0500

    register button group components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@progress/kendo-ui'
+import '@progress/kendo-theme-default/dist/all.css'
+import { ButtonGroup,
+        ButtonGroupButton,
+        ButtonsInstaller } from '@progress/kendo-buttons-vue-wrapper'
+
+Vue.use(ButtonsInstaller)
 
 new Vue({
   el: '#app',
+  components: {
+    ButtonGroup,
+    ButtonGroupButton
+  },
   render: h => h(App)
 })
